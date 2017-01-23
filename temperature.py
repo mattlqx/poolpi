e7924b6fec8e2a9b5a69bdb9aed66b7109915762
Matt Kulka <matt@lqx.net>
2017-01-22 17:49:56 -0700

    add ability to read cpu temperature

--- temperature.py
+++ temperature.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
 # Adapted from http://www.cl.cam.ac.uk/projects/raspberrypi/tutorials/temperature/
+# Run with 'cpu' argument for cpu temp, otherwise read from probe
+
+import sys
 
 temperature_probe_serial = '28-011620f667ee'
+device = "/sys/bus/w1/devices/{serial}/w1_slave".format(serial=temperature_probe_serial)
+do_cpu = False
+
+if len(sys.argv) > 1 and sys.argv[1] == 'cpu':
+    do_cpu = True
+    device = "/sys/class/thermal/thermal_zone0/temp"
 
 # Open temperature probe device
-with open("/sys/bus/w1/devices/{serial}/w1_slave".format(serial=temperature_probe_serial)) as tfile:
+with open(device) as tfile:
     text = tfile.read()
 
 # Parse the data
-secondline = text.split("\n")[1]
-temperaturedata = secondline.split(" ")[9]
-# The first two characters are "t=", so get rid of those and convert the temperature from a string to a number.
-temperature = float(temperaturedata[2:]) / 1000
+if do_cpu:
+    temperaturedata = text
+else:
+    secondline = text.split("\n")[1]
+    # The first two characters are "t="
+    temperaturedata = secondline.split(" ")[9][2:]
+
+temperature = float(temperaturedata) / 1000
 
 # celsius
 #print temperature
